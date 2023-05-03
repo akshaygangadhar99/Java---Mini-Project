9aab30216528944ea2c2945b95cda163a4260c91
akshaygangadhar99 <118504392+akshaygangadhar99@users.noreply.github.com>
2023-05-03 09:43:34 +0530

    .idea

---
+++ Roommate Portal/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    // Remember to add the MySQL connector/ J Driver JAR file to the
+    // project class path
+
+        /*
+        -> For this, right-click on the project, select "Open Module Settings"
+        or press F4
+        -> Select Libraries under the Project Settings section
+        -> Click on the + button and selection "Java"
+        -> Navigate to the location where you downloaded the MySQL Connector/J driver JAR file and select it.
+        -> Click "OK"
+         */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
